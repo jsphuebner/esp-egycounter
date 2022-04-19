64caa60060c3e381db1b296311469d7108189580
johannes <dev@johanneshuebner.com>
2022-04-19 18:59:25 +0200

    Taking voltage from inverter instead of charger, seems better filtered

--- battery-control/netzero.py
+++ battery-control/netzero.py
@@ -5,8 +5,6 @@ import paho.mqtt.client as mqtt
 	
 def setChargePower(client, gridPower):
 	global lastChargerPower
-	global chargerPower
-	global inverterPower
 	
 	solarpower = lastChargerPower - gridPower
 	
@@ -16,7 +14,7 @@ def setChargePower(client, gridPower):
 		client.publish("/charger/setpoint/power", max(0, solarpower - 10))
 	
 def setInverterPower(client, gridPower):
-	global lastChargerVoltage
+	global lastBatteryVoltage
 	global lastChargerPower
 	global maxInverterPower
 
@@ -26,7 +24,7 @@ def setInverterPower(client, gridPower):
 		setInverterPower.errSum = setInverterPower.errSum + gridPower
 		
 	if powerSetpoint > 20 and lastChargerPower < 20:
-		voltagePowerLimit = (lastChargerVoltage - 44) * 500
+		voltagePowerLimit = (lastBatteryVoltage - 44) * 500
 		powerSetpoint = min(voltagePowerLimit, powerSetpoint)
 	else:
 		powerSetpoint = 0
@@ -46,7 +44,7 @@ def publishBatteryPower(client):
 
 def on_message(client, userdata, msg):
 	global lastChargerPower
-	global lastChargerVoltage
+	global lastBatteryVoltage
 	global lastInverterPower
 	global maxInverterPower
 	global lastGridPower
@@ -60,10 +58,10 @@ def on_message(client, userdata, msg):
 			setInverterPower(client, lastGridPower)
 		except ValueError:
 			return
-	elif msg.topic == "/charger/info/voltage":
-		chargerVoltage = float(msg.payload)
-		lastChargerVoltage = (chargerVoltage + lastChargerVoltage * 3) / 4 #IIR Filter
-		client.publish("/battery/voltage", lastChargerVoltage)
+	elif msg.topic == "/inverter/info/voltage":
+		batteryVoltage = float(msg.payload)
+		lastBatteryVoltage = (batteryVoltage + lastBatteryVoltage * 3) / 4 #IIR Filter
+		client.publish("/battery/voltage", lastBatteryVoltage)
 	elif msg.topic == "/charger/info/power":
 		chargerPower = float(msg.payload)
 		lastChargerPower = round((chargerPower + lastChargerPower) / 2, 2) #IIR filter
@@ -91,7 +89,7 @@ maxInverterPower = 0
 lastChargerPower = 0
 lastInverterPower = 0
 lastGridPower = 0
-lastChargerVoltage = 0
+lastBatteryVoltage = 0
 
 setInverterPower.errSum = 0
 
