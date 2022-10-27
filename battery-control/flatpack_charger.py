0170cdf500cbabc3229ec3ac7a46ea4f74f7e738
johannes <dev@johanneshuebner.com>
2022-10-27 11:00:17 +0200

    Improved anti-windup

--- battery-control/flatpack_charger.py
+++ battery-control/flatpack_charger.py
@@ -15,24 +15,23 @@ def on_message(client, hcs, msg):
 	elif msg.topic == "/charger/setpoint/power":
 		power = float(msg.payload)
 		
-		#When transitioning to 0 power preload integrator to a sag of 1V potentially caused by inverter
+		#When transitioning to 0 power preload integrator to a sag of 1.3V potentially caused by inverter
 		if power == 0 and powerSetpoint > 0:
-			powerRegulator.errsum = (voltage - 1) / ki
+			powerRegulator.errsum = (voltage - 1.3) / ki
 			
 		powerSetpoint = power
 		powerTimeout = 20
 		
 def powerRegulator(actualPower, wantedPower, minVoltage, maxVoltage, kp, ki):
 	err = wantedPower - actualPower
-	errSumTemp = powerRegulator.errsum + err
+	powerRegulator.errsum = powerRegulator.errsum + err
+	powerRegulator.errsum = min(powerRegulator.errsum, maxVoltage / ki)
+	powerRegulator.errsum = max(powerRegulator.errsum, minVoltage / ki)
 	
-	output = kp * err + ki * errSumTemp
+	output = kp * err + ki * powerRegulator.errsum
 	outputLimited = min(output, maxVoltage)
 	outputLimited = max(outputLimited, minVoltage)
-	
-	if output == outputLimited:
-		powerRegulator.errsum = errSumTemp
-		
+			
 	return outputLimited
 
 with open("config.json") as configFile:
@@ -47,7 +46,7 @@ client.subscribe("/charger/setpoint/voltage")
 minVoltage = 43
 voltage = minVoltage
 maxVoltage = 0
-maxCurrent = 20
+maxCurrent = 25
 powerSetpoint = 0
 powerTimeout = 0
 kp = 0.001
@@ -67,12 +66,13 @@ while True:
 			#Send walkin
 			msg = can.Message(arbitration_id=0x05004804, data=serial, is_extended_id=True)
 			bus.send(msg)
-			print("Sent walkin to serial " + str(serial))
+			#print("Sent walkin to serial " + str(serial))
 		elif message.arbitration_id == 0x05014004:
 			temperature = message.data[0]
 			current = (message.data[1] + message.data[2] * 256) / 10
 			voltage = (message.data[3] + message.data[4] * 256) / 100
 			power = voltage * current
+			client.publish("/charger/info/temperature", temperature)
 			client.publish("/charger/info/voltage", voltage)
 			client.publish("/charger/info/current", current)
 			client.publish("/charger/info/power", power)
@@ -80,7 +80,7 @@ while True:
 			regulatedVoltage = powerRegulator(power, powerSetpoint, minVoltage, maxVoltage, kp, ki) * 100
 			lobyte = int(regulatedVoltage % 256)
 			hibyte = int(regulatedVoltage / 256)
-			print ("Received status, temperature={0} current={1}, voltage={2}, power={3}, setpoint={4}".format(temperature, current, voltage, power, regulatedVoltage / 100))
+			#print ("Received status, temperature={0} current={1}, voltage={2}, power={3}, setpoint={4}".format(temperature, current, voltage, power, regulatedVoltage / 100))
 			msg = can.Message(arbitration_id=0x05FF4005, data=[maxCurrent * 10, 0, lobyte, hibyte, lobyte, hibyte, lobyte, hibyte + 1] , is_extended_id=True)
 			bus.send(msg)
 	client.loop(timeout=0.1)
