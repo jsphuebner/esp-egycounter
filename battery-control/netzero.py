9ef43d8742344a29b8883f243e8e5b8d73b303c6
johannes <dev@johanneshuebner.com>
2025-02-27 18:56:50 +0100

    Rewrote netzero script to seamlessly switch between V2G and stationary battery

--- battery-control/netzero.py
+++ battery-control/netzero.py
@@ -1,123 +1,92 @@
 #!/usr/bin/python3
 
-import time, sys, json
+import time, sys, json, re
 from datetime import datetime
 import paho.mqtt.client as mqtt
+from picontroller import PiController
 
-def setChargePower(client, gridPower):
-	global lastChargerPower
-	global maxChargePower
-	global lastInverterPower
-	global gridChargeThresh
-	global gridChargePower
-	global price
-	
-	solarpower = lastChargerPower - gridPower
-	solarpower = min(solarpower, maxChargePower)
+def calculateNetzero(ptotal):
+    global config, controller, mqttData
+    
+    if mqttData['sungrow/system_state'] == "Forced Run" and not mqttData['evfull']:
+        chargeLimit = mqttData['pyPlc/target_current'] * mqttData['pyPlc/charger_voltage']
+        chargeLimit = min(chargeLimit, config['netzero']['evpower'])
+        controller.setMinMax(-chargeLimit, config['netzero']['evpower'])
+    else:
+        controller.setMinMax(-min(mqttData['/bms/info/chargepower'], mqttData["/charger/info/maxpower"]),
+                             min(mqttData['/bms/info/dischargepower'], mqttData["/inverter/info/maxpower"]))
+    
+    spnt = controller.run(ptotal, 5)
+    return spnt
 
-	if price < gridChargeThresh:
-		solarpower = min(max(solarpower, gridChargePower), maxChargePower)
-	
-	if lastInverterPower > 10:
-		client.publish("/charger/setpoint/power", 0)
-	else:
-		client.publish("/charger/setpoint/power", max(0, solarpower))
-	
-def setInverterPower(client, gridPower):
-	global config
-	global price
-	global lastBatteryVoltage
-	global lastChargerPower
-	global maxInverterPower
-	global maxDischargePower
-	global lastInverterPower
-	global gridDischargeThresh
-	
-	ki = config['netzero']['powerki']
-	setInverterPower.errSum = setInverterPower.errSum + (gridPower - 10)
-	setInverterPower.errSum = max(0, setInverterPower.errSum)
-	setInverterPower.errSum = min(maxDischargePower / ki, maxInverterPower / ki, setInverterPower.errSum)
+def calculateSpotmarket(netZeroPower):
+    global config, controller, mqttData
 
-	powerSetpoint = (gridPower - 10) * config['netzero']['powerkp'] + setInverterPower.errSum * ki
-	powerSetpoint = min(powerSetpoint, maxInverterPower, maxDischargePower)
-	
-	if price < gridDischargeThresh:
-		powerSetpoint = 0
-	
-	if lastChargerPower > 10 or maxDischargePower < 50:
-	   powerSetpoint = 0
-	   setInverterPower.errSum = 0
-			
-	lastInverterPower = powerSetpoint
-	client.publish("/inverter/setpoint/power", powerSetpoint)
+    price = mqttData["/spotmarket/pricenow"]
+    
+    if mqttData['pyPlc/soc'] >= mqttData['pyPlc/soclimit']:
+        mqttData['evfull'] = True
+    elif mqttData['pyPlc/soc'] < (mqttData['pyPlc/soclimit'] - 3):
+        mqttData['evfull'] = False
+    
+    if mqttData['sungrow/system_state'] == "Forced Run" and not mqttData['evfull']:
+        if mqttData['sungrow/system_state'] == "Forced Run" and price < mqttData["/grid/evchargethresh"]:
+            netZeroPower = -config['netzero']['evpower']
+            controller.resetIntegrator()
+    elif price < mqttData['/grid/chargethresh']:
+        netZeroPower = -mqttData['/grid/chargepower']
+        controller.resetIntegrator()
+    elif price < mqttData['/grid/dischargethresh'] and netZeroPower > 0:
+        netZeroPower = 0
+        controller.resetIntegrator()
+        
+    return netZeroPower
 
-def publishBatteryPower(client):
-	global lastChargerPower
-	global lastInverterPower
-
-	if lastInverterPower > lastChargerPower:
-		client.publish("/battery/power", -lastInverterPower)
-	else:
-		client.publish("/battery/power", lastChargerPower)
-
+def sendChargeDischargeCommand(power):
+    global config, controller, mqttData
+    
+    if mqttData['sungrow/system_state'] == "Forced Run" and not mqttData['evfull']:
+        client.publish("/charger/setpoint/power", 0)
+        client.publish("/inverter/setpoint/power", 0)
+        client.publish("/battery/power", -power)
+        client.publish("/bidi/setpoint/power", -power)
+    elif power < 0: #charge stationary battery
+        client.publish("/charger/setpoint/power", -power)
+        client.publish("/battery/power", -power)
+        client.publish("/inverter/setpoint/power", 0)
+        client.publish("/bidi/setpoint/power", 0)
+    else: #discharge stationary battery
+        client.publish("/charger/setpoint/power", 0)
+        client.publish("/inverter/setpoint/power", power)
+        client.publish("/battery/power", -power)
+        client.publish("/bidi/setpoint/power", 0)
 
 def on_message(client, userdata, msg):
-	global config
-	global price
-	global lastChargerPower
-	global lastBatteryVoltage
-	global lastInverterPower
-	global maxInverterPower
-	global lastGridPower
-	global maxChargePower
-	global maxDischargePower
-	global priceListObtained
-	global gridChargeThresh
-	global gridDischargeThresh
-	global gridChargePower
+    global mqttData
 
-	if msg.topic == config['meter']['topic']:
-		try:
-			data = json.loads(msg.payload)
-			ptotal = data['ptotal']
-			lastGridPower = (ptotal + lastGridPower) / 2
-			setChargePower(client, lastGridPower)
-			setInverterPower(client, lastGridPower)
-			client.publish("/meter/power", ptotal)
-			client.publish("/meter/energy", data['etotal'])
-		except ValueError:
-			return
-	elif msg.topic == "/inverter/info/udc":
-		batteryVoltage = float(msg.payload)
-		lastBatteryVoltage = (batteryVoltage + lastBatteryVoltage * 3) / 4 #IIR Filter
-		client.publish("/battery/voltage", lastBatteryVoltage)
-	elif msg.topic == "/charger/info/power":
-		chargerPower = float(msg.payload)
-		lastChargerPower = round((chargerPower + lastChargerPower) / 2, 2) #IIR filter
-		publishBatteryPower(client)
-	elif msg.topic == "/inverter/info/power":
-		inverterPower = float(msg.payload)		
-		lastInverterPower = (inverterPower + lastInverterPower) / 2 #IIR Filter
-		publishBatteryPower(client)
-	elif msg.topic == "/inverter/info/maxpower":
-		maxInverterPower = float(msg.payload)
-	elif msg.topic == "/bms/info/chargepower":
-		maxChargePower = float(msg.payload)
-	elif msg.topic == "/bms/info/dischargepower":
-		maxDischargePower = float(msg.payload)
-	elif msg.topic == "/grid/chargethresh":
-		gridChargeThresh = float(msg.payload)
-	elif msg.topic == "/grid/dischargethresh":
-		gridDischargeThresh = float(msg.payload)
-	elif msg.topic == "/grid/chargepower":
-		gridChargePower = float(msg.payload)
-	elif msg.topic == "/spotmarket/pricenow":
-		price = float(msg.payload)
+    if msg.topic == config['meter']['topic']:
+        try:
+            data = json.loads(msg.payload)
+            ptotal = data['ptotal']
+            client.publish("/meter/power", ptotal)
+            client.publish("/meter/energy", data['etotal'])
+            ptotal = (mqttData['ptotal'] + ptotal) / 2
+            mqttData['ptotal'] = ptotal
+            power = calculateNetzero(ptotal)
+            power = calculateSpotmarket(power)
+            sendChargeDischargeCommand(power)
+        except ValueError:
+            return
+    else:
+        val = msg.payload.decode("utf-8")
+        if bool(re.match(r'^-?\d+[\.,]*\d*$', val)):
+            val = float(val)
+        mqttData[msg.topic] = val
 
 with open("config.json") as configFile:
 	config = json.load(configFile)
 	
-client = mqtt.Client("netZeroController")
+client = mqtt.Client(client_id = "netZeroController2")
 client.on_message = on_message
 client.connect(config['broker']['address'], 1883, 60)
 client.subscribe(config['meter']['topic'])
@@ -125,29 +94,44 @@ client.subscribe("/charger/info/maxpower")
 client.subscribe("/charger/info/power")
 client.subscribe("/inverter/info/power")
 client.subscribe("/inverter/info/maxpower")
-client.subscribe("/inverter/info/udc")
+client.subscribe("/bms/info/utotal")
 client.subscribe("/bms/info/chargepower")
 client.subscribe("/bms/info/dischargepower")
+client.subscribe("pyPlc/soclimit")
+client.subscribe("pyPlc/soc")
+client.subscribe("pyPlc/charger_voltage")
+client.subscribe("pyPlc/target_current")
+client.subscribe("pyPlc/fsm_state")
 client.subscribe("/grid/chargethresh")
+client.subscribe("/grid/evchargethresh")
 client.subscribe("/grid/dischargethresh")
 client.subscribe("/grid/chargepower")
 client.subscribe("/spotmarket/pricenow")
+client.subscribe("sungrow/system_state")
 
 client.publish("/charger/setpoint/voltage", config['netzero']['chargevoltage'])
 
-maxInverterPower = 0
-lastChargerPower = 0
-lastInverterPower = 0
-lastGridPower = 0
-maxChargePower = 500
-maxDischargePower = 500
-lastBatteryVoltage = config['netzero']['uvlohyst']
-gridChargeThresh = 0
-gridDischargeThresh = 0
-gridChargePower = 0
+mqttData = {
+   '/bms/info/chargepower': 0,
+   '/bms/info/dischargepower': 0,
+   '/grid/evchargethresh"': 0,
+   '/grid/chargethresh': 0,
+   '/grid/dischargethresh': 0,
+   '/grid/chargepower': 0,
+   '/grid/evchargethresh': 0,
+   '/spotmarket/pricenow': 0,
+   'ptotal': 0,
+   'evfull': False,
+   '/charger/info/maxpower': 0,
+   '/inverter/info/maxpower': 0,
+   'pyPlc/fsm_state': 'Off',
+   'pyPlc/charger_voltage': 0,
+   'pyPlc/target_current': 0,
+   'pyPlc/soc': 0,
+   'pyPlc/soclimit': 85,
+   'sungrow/system_state': ''
+}
+controller = PiController(config['netzero']['powerkp'], config['netzero']['powerki'], -1000, 1000)
 
-setInverterPower.errSum = 0
-setInverterPower.uvlo = False
-price = 200
 client.loop_forever()
 
