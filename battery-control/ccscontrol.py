a3195078e3fe25865f099c3d37da7ff1b0694140
johannes <dev@johanneshuebner.com>
2025-11-19 16:32:28 +0100

    Make CCS control pins configurable. Fix to state machine and added precharge control

--- battery-control/ccscontrol.py
+++ battery-control/ccscontrol.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 
 import Adafruit_BBIO.GPIO as GPIO
-import Adafruit_BBIO.PWM as PWM
 import paho.mqtt.client as mqtt
-import time, json
+import time, json, os
 
 def on_message(client, userdata, msg):
-    global ccsState, invState, controlState, switchState
+    global ccsState, invState, controlState, switchState, config
     
     if msg.topic == "pyPlc/fsm_state":
         ccsState = msg.payload.decode("utf-8")
     elif msg.topic == "sungrow/system_state":
         invState = msg.payload.decode("utf-8")
-        
+    
+    if ccsState == "PreCharging":
+        GPIO.output(config["ccsctrl"]["prechargepin"], GPIO.HIGH)
+    else:
+        GPIO.output(config["ccsctrl"]["prechargepin"], GPIO.LOW)
+       
     if controlState == "RunStationary":
-        level = GPIO.input("P8_8")
+        level = GPIO.input(config["ccsctrl"]["ledpin"])
         #toggle LED, sampling switch state is only possible when LED is on
         if level:
-            switchState = GPIO.input("P8_7")
-            GPIO.output("P8_8", GPIO.LOW)            
+            switchState = GPIO.input(config["ccsctrl"]["enablepin"])
+            GPIO.output(config["ccsctrl"]["ledpin"], GPIO.LOW)            
         else:
-            GPIO.output("P8_8", GPIO.HIGH)
+            GPIO.output(config["ccsctrl"]["ledpin"], GPIO.HIGH)
             
         if switchState:
             mqttclient.publish("pyPlc/enabled", 1)
@@ -33,13 +37,13 @@ def on_message(client, userdata, msg):
         client.publish("sungrow/start_stop/set", "Stop")
         controlState = "SwitchToCCS"
     elif controlState == "SwitchToCCS":
-        GPIO.output("P8_8", GPIO.HIGH) #Turn on solid LED
-        if invState == "Stop":
-            GPIO.output("P9_15", GPIO.HIGH) # change over to CCS
+        GPIO.output(config["ccsctrl"]["ledpin"], GPIO.HIGH) #Turn on solid LED
+        if ccsState == "CurrentDemand" and invState == "Stop":
+            GPIO.output(config["ccsctrl"]["changeoverpin"], GPIO.HIGH) # change over to CCS
             client.publish("sungrow/start_stop/set", "Start") #Start inverter
             controlState = "RunCCS"
     elif controlState == "RunCCS":
-        switchState = GPIO.input("P8_7")
+        switchState = GPIO.input(config["ccsctrl"]["enablepin"])
         if not switchState or ccsState != "CurrentDemand":
             controlState = "StopCCS"
     elif controlState == "StopCCS":
@@ -48,20 +52,31 @@ def on_message(client, userdata, msg):
         controlState = "SwitchToStationary"
     elif controlState == "SwitchToStationary":
         if invState == "Stop":
-            GPIO.output("P9_15", GPIO.LOW) # change over to stationary battery
+            GPIO.output(config["ccsctrl"]["changeoverpin"], GPIO.LOW) # change over to stationary battery
             client.publish("sungrow/start_stop/set", "Start") #Start inverter
             controlState = "RunStationary"
             
     client.publish("pyPlc/controlstate", controlState)
 
-GPIO.setup("P8_7", GPIO.IN) #Enable switch
-GPIO.setup("P8_8", GPIO.OUT) #LED of enable switch
-GPIO.setup("P9_23", GPIO.OUT) #HV precharge supply
-GPIO.setup("P9_15", GPIO.OUT) #change over relay between V2G and stationary battery
-PWM.start("P8_13", 95, 1000)
-
 with open("config.json") as configFile:
     config = json.load(configFile)
+
+GPIO.setup(config["ccsctrl"]["enablepin"], GPIO.IN) #Enable switch
+GPIO.setup(config["ccsctrl"]["ledpin"], GPIO.OUT) #LED of enable switch
+GPIO.setup(config["ccsctrl"]["prechargepin"], GPIO.OUT) #HV precharge supply
+GPIO.setup(config["ccsctrl"]["changeoverpin"], GPIO.OUT) #change over relay between V2G and stationary battery
+#PWM.start("P9_42", 95, 1000) #CP PWM
+
+with open(config["ccsctrl"]["pwmbase"] + "period", "w") as f:
+    f.write("1000000")
+    
+with open(config["ccsctrl"]["pwmbase"] + "duty_cycle", "w") as f:
+    f.write("50000")
+    
+with open(config["ccsctrl"]["pwmbase"] + "enable", "w") as f:
+    f.write("1")
+    
+os.system("/usr/bin/config-pin " + config["ccsctrl"]["pwmpin"] + " pwm")
 
 mqttclient = mqtt.Client(client_id = "ccscontrol")
 mqttclient.on_message = on_message
